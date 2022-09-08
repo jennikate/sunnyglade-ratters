cb0611e1f3c8e41cff8aec101d4a92470cc1212d
JenniKate Wallace <jennikate@gmail.com>
2022-09-08 15:16:18 +0100

    Add header component

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import './assets/css/main.scss';
 const App = () => {
   return (
     <>
-      <h1>Basic setup</h1>
-      <div className="logo">
+      <header>
+        <span className="hidden">A World of Warcraft Guild - Argent Dawn EU</span>
+      </header>
+      <div className="brand">
+        <h1>Sunnyglade Ratters</h1>
         <img src="./assets/images/logo.png" />
+        <p>Blurb goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here</p>
       </div>
     </>
   );
