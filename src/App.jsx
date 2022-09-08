f989be1bf5ab2fc20d14bca537cd8cf663633664
JenniKate Wallace <jennikate@gmail.com>
2022-09-08 15:31:05 +0100

    Add member and footer sections

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,19 @@ const App = () => {
   return (
     <>
       <header>
-        <span className="hidden">A World of Warcraft Guild - Argent Dawn EU</span>
+        <span>A World of Warcraft Guild - Argent Dawn EU</span>
       </header>
-      <div className="brand">
+      <section className="brand">
         <h1>Sunnyglade Ratters</h1>
         <img src="./assets/images/logo.png" />
         <p>Blurb goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here goes here</p>
-      </div>
+      </section>
+      <section className="members">
+        <h2>Members</h2>
+      </section>
+      <footer>
+        <p>Characters and likeness belong to their players</p>
+      </footer>
     </>
   );
 };
