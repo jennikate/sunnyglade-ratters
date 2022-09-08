6e15ed17f3f7b32208efc2c45567da15504f484e
JenniKate Wallace <jennikate@gmail.com>
2022-09-08 11:09:44 +0100

    Initial setup

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # sunnyglade-ratters
+
+To run development version
+- `npm i`
+- `npm start`
+
+
+To run a production version
+- `npm run build`
+- ensure you have serve installed (`npm install -g serve`)
+- go to the `dist` folder
+- run `serve -s`
+- view on `http://localhost:5000`
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import App from './App';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+  </React.StrictMode>,
+);
